d800fbdc344a0b3be254968c7cf29710838f6e12
Robinson Regalado <rregalado17@gmail.com>
2019-08-31 22:40:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -86,7 +86,14 @@ class Student
   end
   
   def first_x_students_in_grade_10
+      sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade < 12
+    SQL
     
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
   end
   
 end
