25451f971d6e24f20342f97ff36b5be014147484
Robinson Regalado <rregalado17@gmail.com>
2019-08-31 22:46:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,11 +85,11 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE students = 
+      WHERE students = x
     SQL
     
     DB[:conn].execute(sql).map do |row|
